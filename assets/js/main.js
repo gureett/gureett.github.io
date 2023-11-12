3b4524fe938c136fb4cae0abbc2ec64c47f57797
Mona Lisa <114324924+gureett@users.noreply.github.com>
2023-11-12 17:20:09 -0500

    Add filter functionality and update styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('filters-tab-active')
+        })
+        tab.classList.add('filters-tab-active')
+    })
+})
 
 /*=============== DARK LIGHT THEME ===============*/
 
